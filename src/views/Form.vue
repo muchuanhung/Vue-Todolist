6094606b74ec5f342b796d00b50d6b6e1a4d708e
muchuan <mu.chuan.hung@gmail.com>
2022-01-18 21:26:46 +0800

    feat: todolist第一版

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,32 +1,157 @@
 <template>
   <div id="app">
-    <el-container>
-     <el-row :gutter="10">
-  <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
-  <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
-</el-row>
+    <el-container class="form-container">
+     
+      <div class="form-container__nameinput">
+        <div class="form-container__nameinput-01">
+        <p>
+        姓名
+        </p>
+        <el-input
+         autofocus
+         placeholder="请輸入名字..."
+         v-model="input"
+         @keyup.enter="addItem()"
+        >
+        </el-input>
+        </div>
+      
+        <div class="form-container__nameinput-01">
+        <p>
+        年齡
+        </p>
+        <el-input
+         type="text"
+         placeholder="请輸入年齡..."
+         v-model="newinput"
+        >
+        </el-input>
+        </div>  
+      </div>
+      <div class="form-container__button">
+       <el-button type="primary" class="form-container__button-01"  @click="addItem()">
+         新增
+       </el-button>
+       <el-button  @click="cancelItem()"> 
+         取消
+       </el-button>
+      </div>
+
+      <div class="form-container__form">
+        <el-table
+        :data="tableData"
+        v-if="editingItem !== item"
+        @dblclick="editingItem = item; editingInput = item.content"
+        style="width: 100%">
+        <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+        </el-table-column>
+        <el-table-column
+        prop="age"
+        label="年齡"
+        width="180">
+        </el-table-column>
+        <el-table-column
+        prop="action"
+        label="操作">
+          <el-button type="primary" class="form-container__button-01"  @click="editingItem = item; editingInput = item.content">編輯</el-button>
+          <el-button type="danger" class="form-container__button-01" @click="deleteItem(item)">刪除</el-button>
+        </el-table-column>
+        </el-table> 
+      </div> 
+      
+
+
     </el-container>
   </div>
 </template>
 
 
-<style lang="scss">
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-</style>
+<script>
+    export default {
+      data() {
+        return {
+          tableData: [
+            {
+            name: '王小虎',
+            age: '18',
+            action: '上海市普陀区金沙江路 1518 弄'
+          }, 
+            {
+            name: '王小虎',
+            age: '18',
+            action: '上海市普陀区金沙江路 1518 弄'
+          }, 
+            {
+            name: '王小虎',
+            age: '18',
+            action: '上海市普陀区金沙江路 1518 弄'
+          }
+          ],
+          input: "",
+          editingItem: {},
+          editingInput: {},    
+        }
+      },
+       methods: {
+         addItem() {
+            if (this.input.trim()) {
+                this.list.push({
+                    id: Math.floor(Date.now()),
+                    content: this.input,
+                    done: false,
+                });
+                this.input = '';
+            }
+         },
+         deleteItem: function (target) {
+            let list = this.list;
+            list.forEach((item, index) => {
+                if (item.name === target.name) {
+                    list.splice(index, 1);
+                    return;
+                }
+            });
+         },
+          editItem: function (target) {
+            target.content = this.editingInput;
+            this.editingItem = {};
+        },
+
+      }
+    }
+  </script>
+
+
+
+
+
+  <style lang="scss">
+    .form-container {
+      margin-top: 50px;
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      &__nameinput{
+    
+    }
+      &__nameinput-01{
+    padding: 10px 0;
+    }
+
+    &__button{
+    position: absolute;
+    margin-top: 160px;
+    }
+    &__form{
+    position: absolute;
+    width: 80%;
+    margin-top: 250px;
+    }
+}
+
+
+  </style>
+
